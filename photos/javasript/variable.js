51432b4cf757800e055563501fa9e0fd6537658e
Adhil <adhil.stackup@gmail.com>
2024-02-09 16:09:09 +0530

    initial commit

---
+++ photos/javasript/variable.js
@@ -0,0 +1,15 @@
+var temperature;
+
+
+temperature = " 35"
+
+if (temperature<=20) {
+    console.log("temperature is cold");
+}
+if (temperature<=35) {
+    console.log("temperature is hot");
+}
+else{
+    console.log("very hot ");
+}
+
